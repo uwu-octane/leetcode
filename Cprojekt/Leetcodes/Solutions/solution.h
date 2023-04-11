620dfd42a6ccb4ed5a14cbc6bad3f605db9e8b84
tingting tao <tingting.tao@tu-braunschweig.de>
2023-04-11 20:05:18 +0200

    LC07

--- Cprojekt/Leetcodes/Solutions/solution.h
+++ Cprojekt/Leetcodes/Solutions/solution.h
@@ -5,11 +5,13 @@
 #ifndef LEETCODES_SOLUTION_H
 #define LEETCODES_SOLUTION_H
 
-
+#include <string>
 class solution {
 public:
 int LC07_reverse(int x);
 int LC07_reverse_modified(int x);
+
+int LC08_myAtoi(std::string s);
 };
 
 
